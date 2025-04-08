4af6ee5de78116c36dbb52a1e11ac1dbe58971bd
Srajan-Sanjay-Saxena <srajansaxena@srajans-MacBook-Pro.local>
2025-04-08 19:23:37 +0530

    improved router namings

--- App/app.ts
+++ App/app.ts
@@ -4,9 +4,11 @@ import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
 import { env } from '../newProcess.js';
-import output_false_router from '../Routes/output_false.js'
-import output_true_router from '../Routes/output_true.js'
+
+
 import { globalErrorHandlingMiddleware } from '../controllers/error.controller.js'
+import { writeRouter } from '../Routes/output_false.js'
+import { readRouter } from '../Routes/output_true.js'
 
 const app : Application = express();
 app.use(helmet());
@@ -16,8 +18,8 @@ if(env.NODE_ENV === 'development'){
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended : true }))
-app.use(output_false_router);
-app.use('/api/v1/swarm/read-ops',output_true_router);
+app.use(writeRouter);
+app.use('/api/v1/swarm/read-ops',readRouter);
 
 app.get('/', (req, res)=>{
     res.send('Site working');
--- Routes/output_false.ts
+++ Routes/output_false.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import { deleteFolder, createFile, createFolder, deleteFile } from '../controllers/output_false.js';
 
-const router = express.Router();
+export const writeRouter = express.Router();
 
-router.post('/createFile', createFile);
-router.post('/deleteFile', deleteFile);
-router.post('/createFolder', createFolder);
-router.post('/deleteFolder', deleteFolder);
+writeRouter.post('/createFile', createFile);
+writeRouter.post('/deleteFile', deleteFile);
+writeRouter.post('/createFolder', createFolder);
+writeRouter.post('/deleteFolder', deleteFolder);
 
-export default router;
