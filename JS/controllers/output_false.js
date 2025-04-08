0cfaea3fcbf2d2e42c4f8d0848ceb1867f96ea3a
Siddhant Baranwal <siddhantbr10@gmail.com>
2025-04-08 12:37:06 +0530

    APIs deletefolder, createfolder, createfile, deletefile created

--- JS/controllers/output_false.js
+++ JS/controllers/output_false.js
@@ -1,33 +1,70 @@
 import fs from 'fs';
 import { exec } from 'child_process';
 import path from 'path';
-import os, { homedir } from 'os';
+import os from 'os';
 import { catchAsync } from '../Utils/catchAsync.js';
 import { env } from '../newProcess.js';
-export const createfile = catchAsync(async (req, res, next) => {
-    // Get the home directory
-    const homeDir = os.homedir();
-    // Define full path to the shell script in the home directory
-    const scriptPath = path.join(homeDir, env.SHELL);
-    const filename = req.body.filename;
-    const filePath = path.join(homeDir, filename);
-    console.log(filename);
-    const script = `touch "${filePath}"`;
-    console.log(script);
-    // Step 1: Create the shell script with the content `touch hello.txt`
-    fs.writeFileSync(scriptPath, script, { mode: 0o755 });
-    console.log(`Shell script created at: ${scriptPath}`);
-    // Step 2: Run the shell script
-    exec(`sh ${scriptPath}`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing script: ${error.message}`);
-            return;
+const runCommand = (command, name) => {
+    return new Promise((resolve, reject) => {
+        const homeDir = os.homedir();
+        let filePath = path.join(homeDir, name);
+        if (process.platform === 'win32') {
+            filePath = filePath.replace(/\\/g, '/');
         }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return;
-        }
-        console.log('Shell script executed successfully.');
+        const scriptPath = path.join(homeDir, env.SHELL);
+        const script = `${command} "${filePath}"`;
+        fs.writeFileSync(scriptPath, script, { mode: 0o755 });
+        exec(`sh ${scriptPath}`, (error, stdout, stderr) => {
+            if (error) {
+                // console.error(`Error executing script: ${error.message}`);
+                return reject(new Error(error.message));
+            }
+            if (stderr) {
+                // console.error(`stderr: ${stderr}`);
+                return reject(new Error(stderr));
+            }
+            // console.log('Shell script executed successfully.');
+            resolve();
+        });
     });
-    res.send('Waiting for next command.');
+};
+export const createfile = catchAsync(async (req, res, next) => {
+    try {
+        const filename = req.body.filename;
+        await runCommand('touch', filename);
+        res.send({ 'message': 'success' });
+    }
+    catch (error) {
+        res.send({ 'message': 'failure' });
+    }
+});
+export const deletefile = catchAsync(async (req, res, next) => {
+    try {
+        const filename = req.body.filename;
+        await runCommand('rm', filename);
+        res.send({ 'message': 'success' });
+    }
+    catch (error) {
+        res.send({ 'message': 'failure' });
+    }
+});
+export const createfolder = catchAsync(async (req, res, next) => {
+    try {
+        const foldername = req.body.foldername;
+        await runCommand('mkdir', foldername);
+        res.send({ 'message': 'success' });
+    }
+    catch (error) {
+        res.send({ 'message': 'failure' });
+    }
+});
+export const deletefolder = catchAsync(async (req, res, next) => {
+    try {
+        const foldername = req.body.foldername;
+        await runCommand('rm -rf', foldername);
+        res.send({ 'message': 'success' });
+    }
+    catch (error) {
+        res.send({ 'message': 'failure' });
+    }
 });
